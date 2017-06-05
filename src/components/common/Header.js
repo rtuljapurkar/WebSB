b7c1e5404058e9b755aebc43e00d5b1adc736f45
rtuljapurkar <rtuljapurkar@yahoo.com>
2017-06-04 22:19:33 -0400

    header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -11,7 +11,7 @@ class  Header extends React.Component {
         super();
         this.logOut = this.logOut.bind(this);
         this.state = {
-        scriptString: "alert($(this).parent());console.log($(this));$('.nav').find('.active').removeClass('active');$(this).parent().addClass('active');"
+        scriptString: "$('.nav').find('.current').removeClass('current');$(this).addClass('current');"
     };
     }
 
@@ -47,27 +47,36 @@ render(){
                           <Navbar.Toggle />
                         </Navbar.Header>
                         <Navbar.Collapse>
-                             <Link to="home" className="nav navbar-nav">login</Link>
-                            <ul className="nav navbar-nav" id="ulid">
-                                         <li><a href="/">Home</a></li>
-                                         <li><a href="/venues">Venues</a></li>
 
-                                         <li><a href="/teams">Teams</a></li>
-                                         <li><a href="/posts">Posts</a></li>
-                                         <li className="visible-sm visible-xs"><a href="/favorites">Favorites</a></li>
-                                         <li className="visible-sm visible-xs"><a href="/scores">Scores</a></li>
+                            <ul className="nav navbar-nav" id="ulid">
+                                         <li><Link id="homenav" to="home" className="nav navbar-nav current">Home</Link></li>
+                                         <li><Link id="venuesnav" to="venues" className="nav navbar-nav">Venues</Link></li>
+
+                                         <li><Link id="teamsnav" to="teams" className="nav navbar-nav">Teams</Link></li>
+                                         <li><Link id="postsnav" to="posts" className="nav navbar-nav">Post</Link></li>
+                                         <li className="visible-sm visible-xs"><Link id="favoritesnav" to="favorites" className="nav navbar-nav">Favorites</Link></li>
+                                         <li className="visible-sm visible-xs"><Link id="scoresnav" to="scores" className="nav navbar-nav">Scores</Link></li>
                             </ul>
+                            <ul className="nav navbar-nav">
+                               <li className="dropdown">
+                                 <Link id="privacynav" className="dropdown-toggle" data-toggle="dropdown" to="home">Page 1
+                                 <span className="caret"></span></Link>
+                                 <ul className="dropdown-menu">
+                                   <li><Link id="cookiesnav"  to="posts">Cookie</Link></li>
+                                 </ul>
+                               </li>
+                             </ul>
+                             <li className="dropdown">
+                                <Link id="anav" to="c" className="dropdown-toggle" data-toggle="dropdown">ALink<b className="caret"></b></Link>
+                             		<ul className="dropdown-menu"><li><Link id="anav" to="a" className="nav navbar-nav">ALink</Link></li>
+                             			<li><Link id="bnav" to="b" className="nav navbar-nav">BLink</Link></li>
+                             		</ul></li>
                           <Nav pullRight>
-                              <NavDropdown eventKey={3} title="More" id="basic-nav-dropdown">
-                                <MenuItem eventKey={3.1}>Privacy Policy</MenuItem>
-                                <MenuItem eventKey={3.2}>Cookie Policy</MenuItem>
-                                <MenuItem eventKey={3.3}>Disclaimer</MenuItem>
-                              </NavDropdown>
                               <ul className="nav navbar-nav navbar-right">
-                                <li className="nav"><Link to="/about" >Terms</Link></li>
-                                <li className="nav"><Link to="/about" >About</Link></li>
-                                <li className="nav"><Link to="/about" >Help</Link></li>
-                                <li className="nav"><a href="/logout" onClick={this.logOut}>log out {localStorage.username}</a></li>
+                                <li><Link id="termsnav" to="/about" className="nav navbar-nav" >Terms</Link></li>
+                                <li><Link id="aboutnav" to="/about" className="nav navbar-nav">About</Link></li>
+                                <li><Link id="helpnav" to="/about" className="nav navbar-nav">Help</Link></li>
+                                <li><a href="/logout" onClick={this.logOut}>log out {localStorage.username}</a></li>
                               </ul>
                           </Nav>
                         </Navbar.Collapse>
@@ -132,3 +141,21 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
+// <NavDropdown eventKey={3} title="More" id="morenav" className="nav navbar-nav">
+//   <MenuItem eventKey={3.1} id="privacynav" className="nav navbar-nav">Privacy Policy</MenuItem>
+//   <MenuItem eventKey={3.2} id="cookienav"  className="nav navbar-nav">Cookie Policy</MenuItem>
+//   <MenuItem eventKey={3.3} id="disclaimernav"  className="nav navbar-nav">Disclaimer</MenuItem>
+// </NavDropdown>
+// <li className="dropdown">
+// <Link id="anav" to="c" className="nav navbar-nav dropdown-toggle" data-toggle="dropdown">ALink
+// 		<b className="caret"></b></Link>
+// 		<ul className="dropdown-menu">
+// 			<li>
+//         <Link id="anav" to="a" className="nav navbar-nav">ALink</Link>
+// 			</li>
+// 			<li>
+// 			    <Link id="bnav" to="b" className="nav navbar-nav">BLink</Link>
+// 			</li>
+// 		</ul>
+// 	</li>
